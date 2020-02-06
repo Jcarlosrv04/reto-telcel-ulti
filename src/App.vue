049c9c6ea6d9ec8565d4235cffc1412ad828f1c9
Jcarlosrv04 <58059362+Jcarlosrv04@users.noreply.github.com>
2020-02-06 00:11:26 -0600

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
+
   <div id="app">
+    <h1>Examen Telcel</h1>
     <div class="tabla">
+      <div></div>
       <div>
-         <h3>radiobases</h3>
+         <h2 style="padding-left: 40px;padding-top: 11px;">Radiobases</h2>
       <ul v-for="radiobase in radiobases" :key="id" >
-            {{radiobase.RADIOBASE}}
+      
+            {{radiobase}}
       </ul>
       </div>
       <div>
-         <h3>2019-08-23</h3>
+         <h4 style="padding-left: 40px;">23 de Agosto</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-08-23'" >
+        <span  v-if="trafico.FECHA == '8/23/2019' " >
           <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
@@ -23,12 +27,10 @@
       </ul>
       </div>
       <div>
-         <h3>2019-08-25</h3>
+         <h4 style="padding-left: 40px;" >25 de Agosto</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-08-25'" >
-          
-
-   <div :class="{'rojo' : (trafico.TRAFICO <= 15),
+        <span  v-if="trafico.FECHA ==  '8/25/2019'  " >
+           <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
                          'verde': (trafico.TRAFICO > 90 ),
@@ -44,9 +46,9 @@
       </ul>
       </div>
       <div>
-         <h3>2019-08-27</h3>
+         <h4 style="padding-left: 40px;" >27 de Agosto</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-08-27'" >
+        <span  v-if="trafico.FECHA == '8/27/2019'" >
             <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
@@ -60,9 +62,9 @@
       </ul>
       </div>
       <div>
-         <h3>2019-08-29</h3>
+         <h4 style="padding-left: 40px;" >29 de Agosto</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-08-29'" :class="indicador" >
+        <span  v-if="trafico.FECHA == '8/29/2019'" >
             <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
@@ -76,9 +78,9 @@
       </ul>
       </div>
       <div>
-         <h3>2019-09-01</h3>
+         <h4 style="padding-left: 40px;" >1 de Septiembre</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-09-01'" :class="indicador" >
+        <span  v-if="trafico.FECHA == '9/1/2019'" >
             <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
@@ -92,9 +94,9 @@
       </ul>
       </div>
       <div>
-         <h3>2019-09-03</h3>
+         <h4 style="padding-left: 40px;" >3 de Septiembre</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-09-03'" :class="indicador" >
+        <span  v-if="trafico.FECHA == '9/3/2019' " >
             <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
@@ -108,9 +110,9 @@
       </ul>
       </div>
       <div>
-         <h3>2019-09-04</h3>
+         <h4 style="padding-left: 40px;" >4 de Septiembre</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-09-04'" :class="indicador" >
+        <span  v-if="trafico.FECHA == '9/4/2019' " >
             <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
@@ -124,9 +126,9 @@
       </ul>
       </div>
       <div>
-         <h3>2019-09-07</h3>
+         <h4 style="padding-left: 40px;" >7 de Septiembre</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-09-07'" :class="indicador" >
+        <span  v-if="trafico.FECHA == '9/7/2019'" >
             <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
@@ -140,9 +142,9 @@
       </ul>
       </div>
       <div>
-         <h3>2019-09-08</h3>
+         <h4 style="padding-left: 40px;" >8 de Septiembre</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-09-08'" :class="indicador" >
+        <span  v-if="trafico.FECHA == '9/8/2019'" >
             <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
@@ -156,9 +158,9 @@
       </ul>
       </div>
       <div>
-         <h3>2019-09-09</h3>
+         <h4 style="padding-left: 40px;" >9 de Septiembre</h4>
       <ul v-for="trafico in traficos1" :key="id" >
-        <span  v-if="trafico.FECHA == '2019-09-09'" :class="indicador" >
+        <span  v-if="trafico.FECHA == '9/9/2019'">
             <div :class="{'rojo' : (trafico.TRAFICO <= 15),
                         'naranja': (trafico.TRAFICO > 15 && trafico.TRAFICO <= 40),
                          'amarillo': (trafico.TRAFICO > 40 && trafico.TRAFICO <= 90),
@@ -175,6 +177,10 @@
   </div>
 </template>
 
+
+
+
+
 <script>
 import axios from 'axios'
 
@@ -183,25 +189,24 @@ export default {
     return {
        isActive: true,
       hasError: false,
-      esMenor0_15: null,
-      esMenor15_40: null,
-      esMenor40_90: null,
-      mayor_90: null,
+      
       traficos1:null,
+
       radiobases:[],
       
       
     }
   },
   mounted(){
-    axios.get('http://localhost:4001/telcel')
+    axios.get('http://localhost:4002/telcel')
     .then(res => {
       console.log('res',res)
-      this.radiobases = res.data
-      this.traficos1 = res.data
-      console.log('elementos1',this.traficos1)
+      this.radiobases = res.data[1]
+      this.traficos1 = res.data[0]
       
-      //console.log(this.elementos)
+     // console.log('Trafico',this.traficos1)
+      
+      
     })
   },
   computed: {
@@ -212,19 +217,23 @@ export default {
     }
   }
 }
-
-  
   
 }
 </script>
 
+
+
+
+
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  
 }
 #nav {
   padding: 30px;
@@ -239,10 +248,10 @@ export default {
   color: #42b983;
 }
 
-.mayor15{
-  background-color: red;
-  color: black;
+#h4{
+  text-align: center;
 }
+
 
 .tabla{
   display: flex;
@@ -266,7 +275,5 @@ export default {
 .gris{
   background-color: grey;
 }
-
-
 
 </style>
